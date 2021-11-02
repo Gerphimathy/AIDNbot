7b7098fcf3938378b14f430180a646a7daa29d38
Math <philipponmathis@gmail.com>
2021-11-03 00:02:56 +0100

    Begun Add Reminder function.
    begun add reminder function, information message/output time still needs to be fixed and a link to the database added.

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ var db = mysql.createConnection({
   database: config.mySQLdatabase
 });
 
+
 db.connect(function(err) {
   if (err) throw err;
   else console.log("Connected!");
 });
-
 
 
 //Event listener on messages sent
@@ -53,6 +53,11 @@ client.on("messageCreate", (message) => {
 			//To do: Help Guide
 			break;
 
+		//Add Reminder to Reminder Database
+		case `${config.prefix}reminder`:
+			addReminder(args, message);
+			break;
+
 		//Ping with latency
 		case `${config.prefix}ping`:
 			message.channel.send('🏓 Pong ! **' + (Date.now()- message.createdTimestamp) + 'ms **');
@@ -65,7 +70,7 @@ client.on("messageCreate", (message) => {
 			diceParams[1] = Math.floor(diceParams[1]);
 
 			//Checking the arguments
-			if (diceParams.length != 2) message.channel.send("Invalid argument, please send in this format: **d!throw xdy**");
+			if (diceParams.length != 2) message.channel.send("Invalid argument, please send in this format: **"+`${config.prefix}`+"throw xdy**");
 			else if (diceParams[0] < 1) message.channel.send('Please Send atleast one dice.');
 			else if (diceParams[0] > 10) message.channel.send('10 dies should be more than enough.');
 			else if (diceParams[1] < 2) message.channel.send('You can\'t do probability with only one result.');
@@ -82,9 +87,78 @@ client.on("messageCreate", (message) => {
 			break;
 	}
 
-	//Functions
+});
+
+//general Utility Functions
 	function randomIntInterval(min, max) { // min and max included
 		//taken from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 		return Math.floor(Math.random() * (max - min + 1) + min)
 	}
-});
+
+//Command Related Functions
+	function addReminder(args, messageData) {
+		let isMeasure = true;
+		let i = -1;
+		let totalTime = 0;
+		const measures = ['d','h','m','s'];
+		let setMeasures = [];
+
+		while (isMeasure){
+			i++;
+			if (i != args.length) {
+				let measure = args[i].substr(args[i].length - 1);//get last character of argument
+				let inputTime = parseInt(args[i].substr(0, args[i].length - 1));//get inputed time
+
+				if (!measures.includes(measure)) isMeasure = false;//Will consider this the start of the message if not a measure
+				else if (setMeasures.includes(measure)) isMeasure = false;//Will consider this the start of the message if measure already exists
+				else if (Number.isNaN(inputTime)||inputTime < 0) isMeasure = false; //Will consider this the start of the message if time is invalid
+				else {
+					setMeasures.push(measure);
+					switch (measure) {
+						case 'd':
+							totalTime += 1000 * 3600 * 24 * inputTime;
+							break;
+
+						case 'h':
+							totalTime += 1000 * 3600 * inputTime;
+							break;
+
+						case 'm':
+							totalTime += 1000 * 60 * inputTime;
+							break;
+
+						case 's':
+							totalTime += 60 * inputTime;
+							break;
+
+						default:
+							break;
+					}
+				}//End of else
+			}else isMeasure = false;
+		}//End of While
+		let message = args.slice(i, args.length-1).join(' ');//merge to form message
+		if(i == 0 || i == args.length) messageData.channel.send(
+				"Please send with this format:" +
+				"\n" + `${config.prefix}` + "reminder **Have Atleast one of these**: [xd, yh, xm, ws] [message]." +
+				"\nExample: " + `${config.prefix}` + "reminder 1d 2h remind me !"
+				);
+		else if (message.length > 230) messageData.channel.send("Your message is too long !\nTry trimming it down to 230 characters.");
+		else if (totalTime > 1000*3600*24*30) messageData.channel.send("You cannoy set a reminder due in more than a month.");
+		else if (totalTime == 0) messageData.channel.send("...Really Now ?");
+		else{
+			let timeStamp = messageData.createdTimestamp + totalTime;
+			let timeSeconds = totalTime/1000;
+			let days = Math.floor(totalTime/(3600*24));
+			timeSeconds = timeSeconds%(3600*24);
+			let hours = Math.floor(totalTime/3600);
+			timeSeconds = timeSeconds%3600;
+			let minutes = Math.floor(totalTime/60);
+			let seconds = timeSeconds%60;
+			messageData.channel.send(`Got it, I'll remind you of:\n**${message}**\nIn ${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds.`);
+			//toDO:
+			//register reminder in database
+			//remind user
+			//correct time display of the message above
+		}
+	}//End of addReminder function
